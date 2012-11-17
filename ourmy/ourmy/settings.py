5855b95729fd389031c6f13863874482a28077c0
ChrisBolman <chris@zoomtilt.com>
2012-11-17 17:32:13 -0500

    static images and rel def in settings.py

--- ourmy/ourmy/settings.py
+++ ourmy/ourmy/settings.py
@@ -8,6 +8,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+def rel(*x):
+    return os.path.join(PROJECT_ROOT, *x)
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -69,9 +72,6 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 )
 
 # List of finder classes that know how to find static files in
