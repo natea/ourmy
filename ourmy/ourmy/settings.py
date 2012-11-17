d1b5964c4213cb6623944928265d02c849cf89a7
Anna Callahan <annacallahan@gmail.com>
2012-11-17 15:36:48 -0500

    django debug toolbar

--- ourmy/ourmy/settings.py
+++ ourmy/ourmy/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -139,10 +140,25 @@ INSTALLED_APPS = (
     'singly',
     'ourmy_app',
     'south',
+    'debug_toolbar',
 )
 
 SINGLY_CLIENT_ID = "26c38ca03b256c31720cc9cd8ebec776"
 SINGLY_CLIENT_SECRET = "b174c23ac692e1d0e920e92798ec34c8"
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
