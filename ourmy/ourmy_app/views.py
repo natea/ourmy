4a14f8dcce0a8233198ed048baa778102802018b
Anna Callahan <annacallahan@gmail.com>
2012-11-17 15:36:58 -0500

    index page extends base

--- ourmy/ourmy_app/views.py
+++ ourmy/ourmy_app/views.py
@@ -1,4 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+import models
+from django.http import HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.contrib.sites.models import get_current_site
+from django.template import loader
+import datetime
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+	return render_to_response('index.html', context_instance=RequestContext(request))
