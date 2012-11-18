7e7f5e6d3f2a43027276801292b9c0eb4e4cefa7
Anna Callahan <annacallahan@gmail.com>
2012-11-18 16:39:48 -0500

    create a new bitly url every time you save the Campaign object (in case the long_url changed)

--- ourmy_app/models.py
+++ ourmy_app/models.py
@@ -58,10 +58,9 @@ class CampaignUser(models.Model):
     stats = models.TextField(blank=True, max_length=400)
 
     def save(self, *args, **kwargs):
-        if self.bitly_url is None or self.bitly_url == '':
-            connection = bitly_api.Connection(settings.BITLY_LOGIN, settings.BITLY_API_KEY)
-            result = connection.shorten(self.campaign.long_url)
-            self.bitly_url = result["url"]
+        connection = bitly_api.Connection(settings.BITLY_LOGIN, settings.BITLY_API_KEY)
+        result = connection.shorten(self.campaign.long_url)
+        self.bitly_url = result["url"]
         super(CampaignUser, self).save(*args, **kwargs)      # Call the "real" save() method.
 
 
