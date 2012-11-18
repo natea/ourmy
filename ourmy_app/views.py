40cc07220c6dc259d83cd1e581861a2a9fe408c9
Nate Aune <natea@jazkarta.com>
2012-11-17 21:56:04 -0500

    put authentication stuff on campaign page

--- ourmy_app/views.py
+++ ourmy_app/views.py
@@ -15,18 +15,40 @@ from ourmy_project.settings import SINGLY_CLIENT_ID, SINGLY_CLIENT_SECRET, SINGL
 from django.core import serializers
 
 def index(request):
-	# TODO: check that these are current campaigns
-	current_campaign_list = Campaign.objects.all()
-	return render_to_response('index.html', 
-		{'campaign_list':current_campaign_list},
-		context_instance=RequestContext(request))
+    # TODO: check that these are current campaigns
+    current_campaign_list = Campaign.objects.all()
+    return render_to_response('index.html', 
+        {'campaign_list':current_campaign_list},
+        context_instance=RequestContext(request))
 
 
 def campaign(request, campaign_id):
-	campaign = get_object_or_404(Campaign, pk=campaign_id)
-	return render_to_response('campaign.html',
-		{'campaign':campaign},
-		context_instance=RequestContext(request))
+    campaign = get_object_or_404(Campaign, pk=campaign_id)
+
+    services = [
+        'Facebook',
+        # 'foursquare',
+        # 'Instagram',
+        # 'Tumblr',
+        'Twitter',
+        'LinkedIn',
+        # 'FitBit',
+        # 'Email'
+    ]
+ 
+    if request.user.is_authenticated():
+        try:
+            user_profile = request.user.get_profile()
+            # We replace single quotes with double quotes b/c of python's strict json requirements
+            profiles = simplejson.loads(user_profile.profiles.replace("'", '"'))
+        except:
+            pass
+
+    response = render_to_response('campaign.html',
+         locals(),
+         context_instance=RequestContext(request)
+        )
+    return response
 
 def connect(request, template='connect.html'):
     services = [
@@ -63,11 +85,6 @@ def post(request, template='post.html'):
                'url': 'http://singly.com'}
 
     success = singly.make_request('/types/news', method='POST', request=payload)
- 
-
-    # response_dict = serializers.deserialize("json", response)
-
-    print success
 
     response = render_to_response(
             template, locals(), context_instance=RequestContext(request)
