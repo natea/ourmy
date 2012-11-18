8da98dca4a38332087ec653c0cd4b9c0684e7dfd
Anna Callahan <annacallahan@gmail.com>
2012-11-18 12:08:01 -0500

    dealing with anonymous users

--- ourmy_app/admin.py
+++ ourmy_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from ourmy_app.models import Campaign, Action, Prize, CampaignUser
+from ourmy_app.models import Campaign, Action, Prize, CampaignUser, UserActions
 
 admin.site.register(Campaign)
 admin.site.register(Action)
 admin.site.register(Prize)
 admin.site.register(CampaignUser)
+admin.site.register(UserActions)
--- ourmy_app/views.py
+++ ourmy_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from ourmy_app.models import Campaign, Action, CampaignUser
+from ourmy_app.models import Campaign, Action, CampaignUser, UserActions
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.contrib.sites.models import get_current_site
@@ -12,8 +12,11 @@ import simplejson
 from singly.singly import Singly
 from urllib import urlencode
 from ourmy_project.settings import SINGLY_CLIENT_ID, SINGLY_CLIENT_SECRET, SINGLY_REDIRECT_URI
+from django.conf import settings
 from django.core import serializers
 import random
+import bitly_api
+from django.utils.functional import LazyObject
 
 def index(request):
     # TODO: check that these are current campaigns
@@ -45,13 +48,31 @@ def campaign(request, campaign_id):
         except:
             pass
 
+    # create a CampaignUser object - this creates the unique bitly for this user for this campaign
+    if isinstance(request.user, LazyObject):
+        user = User(first_name="anonymous", username="anonymous%d" % random.randrange(1,1000000))
+        user.save()
+    else:
+        user = request.user
+    
+    campaign_user, created = CampaignUser.objects.get_or_create(user=user, campaign=campaign)
+    campaign_user.save()
+
     users = User.objects.all()
     for user in users:
         user.points = random.randrange(1,100)
+        # get all the actions this user has done
 
-    # create a CampaignUser object - this creates the unique bitly for this user for this campaign
-    campaign_user, created = CampaignUser.objects.get_or_create(user=request.user, campaign=campaign)
-    campaign_user.save()
+        user.points = 0
+        user_actions = UserActions.objects.filter(user=user)
+        # the user gets points for posting
+        for user_action in user_actions:
+            user.points += user_action.action.points_to_post
+        # calculate points for each time their link was clicked
+        # TODO: make this based on which social network it is
+        connection = bitly_api.Connection(settings.BITLY_LOGIN, settings.BITLY_API_KEY)
+        result = connection.clicks(campaign_user.bitly_url)
+        # user.points += result["clicks"]*user_actions[0]
 
     response = render_to_response('campaign.html',
          locals(),
@@ -95,6 +116,11 @@ def post(request, template='post.html'):
                'url': 'http://singly.com'}
 
     success = singly.make_request('/types/news', method='POST', request=payload)
+
+    # if they have posted, we create a UserAction for them and store it in the database
+    # if success:
+    #     action, created = Action.objects.get_or_create(campaign=)
+    #     user_action = UserActions(user=request.user, action=)
 
     response = render_to_response(
             template, locals(), context_instance=RequestContext(request)
