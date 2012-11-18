3484c4250ed1a222350112901b1d174dd575aee7
Anna Callahan <annacallahan@gmail.com>
2012-11-17 19:00:54 -0500

    adding Prizes and logos for Campaign objects

--- ourmy_app/admin.py
+++ ourmy_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from ourmy_app.models import Campaign, Action
+from ourmy_app.models import Campaign, Action, Prize
 
 admin.site.register(Campaign)
 admin.site.register(Action)
+admin.site.register(Prize)
