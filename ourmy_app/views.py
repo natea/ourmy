25c29b407366191c288ee27ca07c6922db7b711c
Anna Callahan <annacallahan@gmail.com>
2012-12-06 16:48:24 -0500

    test of a login page...

--- ourmy_app/views.py
+++ ourmy_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from ourmy_app.models import Campaign, Action, CampaignUser, UserActions
+from ourmy_app.models import Campaign, Action, CampaignUser, UserAction
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.contrib.sites.models import get_current_site
@@ -20,7 +20,8 @@ import bitly_api
 from django import forms
 from ourmy_app.forms import CampaignForm
 from django.utils.functional import LazyObject
-from social_networking import get_points_for_user
+from sharing import get_points_for_user
+from sharing.models import SharingCampaign, SharingCampaignUser
 
 
 def index(request):
@@ -30,12 +31,11 @@ def index(request):
         {'campaign_list':current_campaign_list},
         context_instance=RequestContext(request))
 
-
+@login_required
 def create_campaign(request, campaign_id=None):
     instance=None
     campaigns = []
-    if request.user.is_authenticated():
-        campaigns = Campaign.objects.filter(user=request.user)
+    campaigns = Campaign.objects.filter(user=request.user)
     if request.method == 'POST':
         if campaign_id is not None:
             instance = get_object_or_404(Campaign, pk=campaign_id)
@@ -60,6 +60,8 @@ def create_campaign(request, campaign_id=None):
 
 def campaign(request, campaign_id):
     campaign = get_object_or_404(Campaign, pk=campaign_id)
+    sharing_campaign_user = None
+    profiles = None
 
     services = [
         'facebook',
@@ -71,29 +73,14 @@ def campaign(request, campaign_id):
         # 'FitBit',
         # 'Email'
     ]
- 
-    campaign_user = None
-    if request.user.is_authenticated():
-        user = request.user
-        # create a CampaignUser object - this creates the unique bitly for this user for this campaign      
-        campaign_user, created = CampaignUser.objects.get_or_create(user=user, campaign=campaign)
-        campaign_user.save()
-        try:
-            user_profile = request.user.get_profile()
-            # We replace single quotes with double quotes b/c of python's strict json requirements
-            profiles = simplejson.loads(user_profile.profiles.replace("'", '"'))
-        except:
-            pass
-    
-    # campaign_user, created = CampaignUser.objects.get_or_create(user=user, campaign=campaign)
-    # campaign_user.save()
 
+    # Leaderboard
     users = User.objects.all()
     for user in users:
         # get all the actions this user has done
 
         user.points = 0
-        user_actions = UserActions.objects.filter(user=user)
+        user_actions = UserAction.objects.filter(user=user)
         # the user gets points for posting
         for user_action in user_actions:
             user.points += user_action.action.points_to_post
@@ -104,6 +91,31 @@ def campaign(request, campaign_id):
         # user.points += result["clicks"]*user_actions[0]
         user.points += get_points_for_user(user)
     sorted_users = sorted(users, key=lambda o:o.points, reverse=True)
+
+ 
+    # Bitly sharing link
+    campaign_user = None
+    if request.user.is_authenticated():
+        user = request.user
+        # create a CampaignUser object - this creates the unique bitly for this user for this campaign      
+        campaign_user, created = CampaignUser.objects.get_or_create(user=user, campaign=campaign)
+        if created: print "the campaign user was created"
+        campaign_user.save()
+        sharing_campaign, created = SharingCampaign.objects.get_or_create(campaign=campaign)
+        if created: print "the campaign user was created"
+        sharing_campaign_user, created = SharingCampaignUser.objects.get_or_create(sharing_campaign=sharing_campaign, user=user)
+        if created: 
+            print "the campaign user was created"
+        sharing_campaign_user.save()
+        try:
+            user_profile = request.user.get_profile()
+            # We replace single quotes with double quotes b/c of python's strict json requirements
+            profiles = simplejson.loads(user_profile.profiles.replace("'", '"'))
+        except:
+            pass
+    
+    # campaign_user, created = CampaignUser.objects.get_or_create(user=user, campaign=campaign)
+    # campaign_user.save()
 
     if request.method == 'POST':
         singly = Singly(SINGLY_CLIENT_ID, SINGLY_CLIENT_SECRET)
@@ -128,7 +140,7 @@ def campaign(request, campaign_id):
                 try:
                     success = return_data[service]['id']
                     action, created = Action.objects.get_or_create(campaign=campaign, social_network=service)
-                    user_action = UserActions(user=request.user, action=action)
+                    user_action = UserAction(user=request.user, action=action)
                     user_action.save()
                 except:
                     pass
@@ -139,7 +151,8 @@ def campaign(request, campaign_id):
         # if success:
 
     response = render_to_response('campaign.html',
-         { 'campaign':campaign, 'user':request.user, 'services':services, 'users':sorted_users, 'campaign_user':campaign_user },
+         { 'campaign':campaign, 'user':request.user, 'services':services, 
+           'users':sorted_users, 'sharing_campaign_user':sharing_campaign_user, 'profiles':profiles },
          context_instance=RequestContext(request)
         )
     return response
@@ -162,5 +175,24 @@ def connect(request, template='connect.html'):
         profiles = simplejson.loads(user_profile.profiles.replace("'", '"'))
     response = render_to_response(
             template, locals(), context_instance=RequestContext(request)
+        )
+    return response
+
+def login(request):
+    services = [
+        'Facebook',
+        # 'foursquare',
+        # 'Instagram',
+        # 'Tumblr',
+        'Twitter',
+        # 'LinkedIn',
+        # 'FitBit',
+        # 'Email'
+    ]
+    if request.user.is_authenticated():
+        user_profile = request.user.get_profile()
+        # We replace single quotes with double quotes b/c of python's strict json requirements
+        profiles = simplejson.loads(user_profile.profiles.replace("'", '"'))
+    response = render_to_response('login.html', locals(), context_instance=RequestContext(request)
         )
     return response
--- ourmy_project/urls.py
+++ ourmy_project/urls.py
@@ -19,6 +19,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    url(r'^login/$', 'ourmy_app.views.campaign', name='login'),
+
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 	# url("^$", direct_to_template, {"template": "index.html"}, name="home"),
