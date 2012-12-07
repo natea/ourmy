9e0660f26217482dfcc7f2f60ce25ace297f59b8
Anna Callahan <annacallahan@gmail.com>
2012-12-06 20:52:28 -0500

    migration for moving api call from campaign user to campaign

---
+++ ourmy_app/migrations/0006_auto__add_field_campaign_api_call__del_field_campaignuser_api_call.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Campaign.api_call'
+        db.add_column('ourmy_app_campaign', 'api_call',
+                      self.gf('django.db.models.fields.CharField')(default='?', max_length=500),
+                      keep_default=False)
+
+        # Deleting field 'CampaignUser.api_call'
+        db.delete_column('ourmy_app_campaignuser', 'api_call')
+
+
+    def backwards(self, orm):
+        # Deleting field 'Campaign.api_call'
+        db.delete_column('ourmy_app_campaign', 'api_call')
+
+        # Adding field 'CampaignUser.api_call'
+        db.add_column('ourmy_app_campaignuser', 'api_call',
+                      self.gf('django.db.models.fields.CharField')(default='?', max_length=500),
+                      keep_default=False)
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'ourmy_app.action': {
+            'Meta': {'object_name': 'Action'},
+            'api_call': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'campaign': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ourmy_app.Campaign']"}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'end_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_checked': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'logo_image': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'points': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'start_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'video_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'ourmy_app.campaign': {
+            'Meta': {'object_name': 'Campaign'},
+            'api_call': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'deadline': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '250', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo_image': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'video_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'ourmy_app.campaignuser': {
+            'Meta': {'object_name': 'CampaignUser'},
+            'campaign': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ourmy_app.Campaign']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_checked': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'ourmy_app.prize': {
+            'Meta': {'object_name': 'Prize'},
+            'campaign': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ourmy_app.Campaign']"}),
+            'chance': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '250', 'blank': 'True'}),
+            'dollar_value': ('django.db.models.fields.DecimalField', [], {'default': '10', 'max_digits': '6', 'decimal_places': '2'}),
+            'how_many': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo_image': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'place': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'points_value': ('django.db.models.fields.IntegerField', [], {'default': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'video_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'ourmy_app.useraction': {
+            'Meta': {'object_name': 'UserAction'},
+            'action': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ourmy_app.Action']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'ourmy_app.userprofile': {
+            'Meta': {'object_name': 'UserProfile', '_ormbases': ['singly.SinglyProfile']},
+            'bio': ('django.db.models.fields.TextField', [], {}),
+            'singly_profile': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'OurmyUserProfile'", 'unique': 'True', 'to': "orm['singly.SinglyProfile']"}),
+            'singlyprofile_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['singly.SinglyProfile']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'singly.singlyprofile': {
+            'Meta': {'object_name': 'SinglyProfile', 'db_table': "'user_profile'"},
+            'access_token': ('django.db.models.fields.CharField', [], {'max_length': '260', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'profiles': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'singly_id': ('django.db.models.fields.CharField', [], {'max_length': '260', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'profile'", 'to': "orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['ourmy_app']
