07f5cda7842b5419b777f838cdd755bdbc10d920
Anna Callahan <annacallahan@gmail.com>
2012-12-06 20:52:42 -0500

    We're now getting points from the sharing app

--- ourmy_app/views.py
+++ ourmy_app/views.py
@@ -20,8 +20,10 @@ import bitly_api
 from django import forms
 from ourmy_app.forms import CampaignForm
 from django.utils.functional import LazyObject
+import sharing
 from sharing import get_points_for_user
 from sharing.models import SharingCampaign, SharingCampaignUser
+from annoying.functions import get_object_or_None
 
 
 def index(request):
@@ -77,19 +79,48 @@ def campaign(request, campaign_id):
     # Leaderboard
     users = User.objects.all()
     for user in users:
-        # get all the actions this user has done
-
         user.points = 0
+        # get all the actions this user has done by pinging the CampaignUser's api call
+        # TODO: should we delete any user_actions that are not returned by this call?
+        campaign_user = get_object_or_None(CampaignUser, pk=user.id)
+        if campaign_user:
+            words = campaign_user.campaign.api_call.split(".")
+            module = __import__(words[0])
+            funct = getattr(module, words[1])
+            list_of_actions_ids = funct(user)
+            # for each element, get or create a new UserAction.
+            for action_id in list_of_actions_ids:
+                action = get_object_or_None(Action, api_call=action_id)
+                # create any actions that don't exist
+                if action:
+                    user_action, created = UserAction.get_or_create(user=user, action=action)
+                    user_action.save()
+
         user_actions = UserAction.objects.filter(user=user)
-        # the user gets points for posting
+
         for user_action in user_actions:
-            user.points += user_action.action.points_to_post
+            # call the api for this action.  This returns how many of these actions this user did.
+            points_call = user_action.action.api_call
+            words = points_call.split(".")
+            module = __import__(words[0])
+            funct = getattr(module, words[1])
+            user.points += user_action.action.points * funct(user)
+
+
         # calculate points for each time their link was clicked
         # TODO: make this based on which social network it is
         # connection = bitly_api.Connection(settings.BITLY_LOGIN, settings.BITLY_API_KEY)
         # result = connection.clicks(campaign_user.bitly_url)
         # user.points += result["clicks"]*user_actions[0]
-        user.points += get_points_for_user(user)
+        # user.points += get_points_for_user(user)
+        # points_calls = ["sharing.get_facebook_post_points_for_user", "sharing.get_facebook_click_points_for_user", "sharing.get_twitter_post_points_for_user", "sharing.get_twitter_click_points_for_user"]
+
+        # points_calls = Action.objects.filter(campaign=campaign).values_list('api_call', flat=True)
+        # for element in points_calls:
+        #     words = element.split(".")
+        #     module = __import__(words[0])
+        #     funct = getattr(module, words[1])
+        #     user.points += funct(user)
     sorted_users = sorted(users, key=lambda o:o.points, reverse=True)
 
  
--- sharing/__init__.py
+++ sharing/__init__.py
@@ -13,3 +13,22 @@ def get_points_for_user(user, *args, **kwargs):
 	# # For each VSUserAction that is a click, return the bitly count.
 	# for sn in SharingUserAction.SOCIAL_NETWORKS:
 	# 	click_user_actions = SharingUserAction.objects.filter(user_action.user=user, social_network=sn, post_or_clicked=1)
+
+def get_actions_for_user(user):
+	return ["get_facebook_post_points_for_user", "get_facebook_click_points_for_user", "get_twitter_post_points_for_user", "get_twitter_click_points_for_user"]
+
+def get_facebook_post_points_for_user(user, *args, **kwargs):
+	print "called get_facebook_post_points_for_user"
+	return random.randrange(1,100)
+
+def get_facebook_click_points_for_user(user, *args, **kwargs):
+	print "called get_facebook_click_points_for_user"
+	return random.randrange(1,100)
+
+def get_twitter_post_points_for_user(user, *args, **kwargs):
+	print "called get_twitter_post_points_for_user"
+	return random.randrange(1,100)
+
+def get_twitter_click_points_for_user(user, *args, **kwargs):
+	print "called get_twitter_click_points_for_user"
+	return random.randrange(1,100)
