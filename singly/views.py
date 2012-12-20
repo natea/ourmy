52be0bd587d133f3e3a6d2d8cc0d1c177f9040d9
Anna Callahan <annacallahan@gmail.com>
2012-12-20 16:44:42 -0600

    Generate a random password instead of using "fake password" (that was from singly code)

--- singly/views.py
+++ singly/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from singly import SinglyHelper
 from django.contrib.auth import authenticate, login as auth_login
 from models import SinglyProfile
@@ -16,5 +17,15 @@ def authorize_callback(request):
             content['account'], content['access_token'])
     if not request.user.is_authenticated():
         user = authenticate(username=user_profile.user.username, password='fakepassword')
+        password = User.objects.make_random_password()
+        user.set_password(password)
         auth_login(request, user)
-    return HttpResponseRedirect('/campaign/1')
+
+    DEFAULT_URL = 'http://example.com/wherever/'
+    destination = request.META.get('HTTP_REFERER', DEFAULT_URL)
+    MY_BASE_URL = 'http://zoomtilt.com/' # trailing slash important
+    LOCALLY = "http://localhost:"
+    if destination.startswith(MY_BASE_URL) or destination.startswith(LOCALLY):
+        return HttpResponseRedirect(destination)
+    else:
+        return HttpResponseRedirect('/')
