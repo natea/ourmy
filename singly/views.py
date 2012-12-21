6ff8e2bac69cb6fcb35945cae6a40916a9939179
Anna Callahan <annacallahan@gmail.com>
2012-12-21 16:32:42 -0600

    Redirect the user back to the campaign they were on before, not a hard-coded campaign/1.

--- singly/views.py
+++ singly/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from singly import SinglyHelper
 from django.contrib.auth import authenticate, login as auth_login
 from models import SinglyProfile
+from django.contrib.auth.models import User
 
 
 def authenticate_redirect(request, service):
@@ -15,6 +16,20 @@ def authorize_callback(request):
     user_profile = SinglyProfile.objects.get_or_create_user(
             content['account'], content['access_token'])
     if not request.user.is_authenticated():
+        # ideally, we would be using a randomized password.  Not sure why this isn't working
+        # password = User.objects.make_random_password()
+        # print "password: " + password
+        # user = authenticate(username=user_profile.user.username, password=password)
         user = authenticate(username=user_profile.user.username, password='fakepassword')
         auth_login(request, user)
-    return HttpResponseRedirect('/campaign/1')
+    # return HttpResponseRedirect('/campaign/1')
+
+    # we want to redirect the user back to the campaign they were on before.
+    DEFAULT_URL = 'http://www.zoomtilt.com/'
+    destination = request.META.get('HTTP_REFERER', DEFAULT_URL)
+    MY_BASE_URL = 'http://www.zoomtilt.com/' # trailing slash important
+    LOCALLY = "http://localhost:"
+    if destination.startswith(MY_BASE_URL) or destination.startswith(LOCALLY):
+        return HttpResponseRedirect(destination)
+    else:
+        return HttpResponseRedirect('/')
