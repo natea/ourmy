e97ac6caabc859e863760035099d9f8fa9d24192
Anna Callahan <annacallahan@gmail.com>
2013-01-18 13:07:37 -0500

    notes on Singly

---
+++ singly/__init__.py
@@ -0,0 +1,9 @@
+# both of our python examples are updated as of a month ago
+# here is a module: https://github.com/Singly/django-singly
+	
+# Singly/django-singly
+# A django auth backend app for Singly
+# here is an example app: https://github.com/Singly/python_django_skeleton
+	
+# Singly/python_django_skeleton
+# An example app using Singly, built with django
